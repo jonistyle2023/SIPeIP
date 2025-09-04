accee7bb11506c5a82bac6b0f09a0e107cdfba29
jonistyle2023 <jonathanpan22@gmail.com>
2025-09-04 11:42:34 -0500

    fix bug with header

--- react-app/src/features/investment-projects/ProjectDetailModal.jsx
+++ react-app/src/features/investment-projects/ProjectDetailModal.jsx
@@ -4,7 +4,7 @@ import ProjectDetail from './ProjectDetail.jsx';
 
 export default function ProjectDetailModal({project, onClose}) {
     return (
-        <div className="fixed inset-0 bg-white/30 backdrop-blur-sm flex justify-center items-center z-50 p-4">
+        <div className="fixed inset-0 bg-white/30 backdrop-blur-sm flex justify-center items-center z-80 p-4">
             <div className="bg-white rounded-lg shadow-xl w-full max-w-6xl h-[90vh] flex flex-col">
                 <div className="p-4 border-b flex justify-between items-center">
                     <h3 className="text-lg font-semibold">Detalle del Proyecto Postulado</h3>
